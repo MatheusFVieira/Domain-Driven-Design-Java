8d70002e330e60e3b75a768def3b75412e07e135
MatheusFVieira <matheus.vf06@gmail.com>
2026-03-16 22:31:03 -0300

    Aula_2

---
+++ Aula_2/src/Aluno.java
@@ -0,0 +1,12 @@
+public class Aluno {
+
+        String nome;
+        int idade;
+        String curso;
+
+        void apresentar(){
+            System.out.println("O nome do Aluno é " + nome);
+            System.out.println("A idade do Aluno é " + idade);
+            System.out.println("O curso do Aluno é " + curso + "\n");
+        }
+}
