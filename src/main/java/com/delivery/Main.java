1b1b365685e8597866c6341486eb436fa4cb8c2b
MatheusFVieira <matheus.vf06@gmail.com>
2026-04-06 21:43:24 -0300

    Aula_4

---
+++ src/main/java/com/delivery/Main.java
@@ -0,0 +1,12 @@
+package com.delivery;
+
+// Importando as classes que criamos
+import com.delivery.cliente.Cliente;
+import com.delivery.cliente.Endereco;
+import com.delivery.pedido.Pedido;
+import com.delivery.pedido.ItemPedido;
+
+public class Main {
+
+
+}
